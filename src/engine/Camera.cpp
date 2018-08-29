959d638c0103d3ddb2ccdf5f293f62dedd4bc070
zmertens <zmertens@asu.edu>
2018-08-29 14:06:36 -0700

    Fix missing GLM_ENABLE_EXPERIMENTAL defines for recent glm version \ Camera, Transform classes

--- src/engine/Camera.cpp
+++ src/engine/Camera.cpp
@@ -1,5 +1,6 @@
 #include "Camera.hpp"
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
--- src/engine/Transform.cpp
+++ src/engine/Transform.cpp
@@ -1,5 +1,6 @@
 #include "Transform.hpp"
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
