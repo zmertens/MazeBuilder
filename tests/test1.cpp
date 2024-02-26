5062657c173812745136188bdfe78095bb0ad8ea
zmertens <zach.mm035@gmail.com>
2024-02-26 05:27:42 -0700

    Update test to only link against maze algo files (not opengl, sdl, ...)

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -6,10 +6,11 @@
 #include "../sources/bst_maze.h"
 
 unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+    return (number == 1 || number == 0) ? 1 : Factorial(number-1)*number;
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
