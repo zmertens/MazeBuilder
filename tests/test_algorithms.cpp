adf09ed6774b2b8c444464d8ed822eb9046f4329
zmertens <zach.mm035@gmail.com>
2024-03-10 11:58:36 -0700

    Start adding threading tests to algorithms

--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -6,6 +6,7 @@
 #include <functional>
 #include <random>
 #include <future>
+#include <thread>
 
 #include "maze_algo_interface.h"
 #include "grid.h"
@@ -22,6 +23,11 @@ static auto get_int = [](int low, int high) ->int {
     uniform_int_distribution<int> dist {low, high};
     return dist(mt);
 };
+
+// static auto append_grids = [](grid_ptr& g1, grid_ptr& g2, grid_ptr& g12) -> void {
+    // g12.reset();
+    // g12 = {make_unique<grid>(g1->get_rows() * g2->get_rows(), g1->get_columns() * g2->get_columns())};
+// };
 
 TEST_CASE("Compare maze algos", "[maze_algos_race]") {
     grid_ptr _grid_from_bt {make_unique<grid>(500, 500)};
@@ -55,11 +61,6 @@ TEST_CASE("Compare maze algos", "[maze_algos_race]") {
         ss.clear();
         ss << *_grid_from_sw.get();
         REQUIRE(ss.str().length() != 0);
-        // for (auto i {0}; i < _grid_from_bt->get_rows(); i++) {
-        //     for (auto j {0}; j < _grid_from_bt->get_columns(); j++) {
-        //         REQUIRE(_grid_from_bt->get_grid().at(i).at(j) != nullptr);
-        //     }
-        // }
     }
 }
 
@@ -87,4 +88,38 @@ TEST_CASE("Cells have neighbors", "[cells]") {
         cell1->link(cell1, cell2);
         REQUIRE(cell1->is_linked(cell2));
     }
+}
+
+TEST_CASE("Threading grids", "[threading]") {
+    grid_ptr _grid1 {make_unique<grid>(250, 250)};
+    grid_ptr _grid2 {make_unique<grid>(250, 250)};
+    grid_ptr _grid3 {make_unique<grid>(250, 250)};
+    grid_ptr _grid4 {make_unique<grid>(250, 250)};
+    
+    auto run_bt = [&](grid_ptr& g)->bool {
+        binary_tree bt;
+        return bt.run(ref(g), get_int, false);
+    };
+    
+    // thread t1 (run_bt, _grid1);
+    // thread t2 (run_bt, _grid2);
+    // thread t3 (run_bt, _grid3);
+    // thread t4 (run_bt, _grid4);
+    // t1.join();
+    // t2.join();
+    // t3.join();
+    // t4.join();
+
+    // grid_ptr _grid12;
+    // append_grids(_grid1, _grid2, _grid12);
+    // grid_ptr _grid34; = move(append_grids(_grid3, _grid4));
+    // grid_ptr _grid_final = move(append_grids(_grid12, _grid34));
+
+    // stringstream ss;
+    // ss << *_grid12.get();
+    // REQUIRE(ss.str().empty() == false);
+    // REQUIRE(_grid12->get_rows() >= _grid1->get_rows());
+    // REQUIRE(_grid12->get_rows() >= _grid2->get_rows());
+    // REQUIRE(_grid12->get_columns() >= _grid1->get_columns());
+    // REQUIRE(_grid12->get_columns() >= _grid2->get_columns());
 }
