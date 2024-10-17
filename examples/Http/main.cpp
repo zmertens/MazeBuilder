576b08277881f81c594587ccfbeddd06978c70e1
zmertens <zach.mm035@gmail.com>
2024-10-17 10:36:09 -0700

    Update input processing and display - fix uninitialized http error

--- examples/Http/main.cpp
+++ examples/Http/main.cpp
@@ -17,7 +17,7 @@
 
 #include <MazeBuilder/maze_builder.h>
 
-static std::shared_ptr<mazes::maze_builder> my_maze_builder;
+static std::shared_ptr<mazes::maze_builder> my_maze_builder = std::make_shared<mazes::maze_builder>(1, 1, 1);
 
 static constexpr auto MB_MENU_MSG = R"help(
     -- Maze Builder Menu --
@@ -29,10 +29,16 @@ static constexpr auto MB_MENU_MSG = R"help(
     [version: 0.1.0]
     )help";
 
+static constexpr auto WORKER_URL = "hello-wrangler.zach-mm035.workers.dev";
+
+void print_menu() {
+    std::cout << MB_MENU_MSG;
+    std::cout << "\nHttp URL: " << WORKER_URL << std::endl;
+}
+
 void process_commands(std::deque<char>& commands,
     bool& is_running,
-    const std::shared_ptr<mazes::maze_builder>& mb,
-    const std::string& WORKER_URL) {
+    const std::shared_ptr<mazes::maze_builder>& mb) {
     
     using namespace std;
 
@@ -42,12 +48,12 @@ void process_commands(std::deque<char>& commands,
         return dist(rng);
     };
 
-    auto process_char = [&mb, &is_running, &WORKER_URL, &get_int, &rng](char ch) {
+    auto process_char = [&mb, &is_running, &get_int, &rng](char ch) {
         sf::Http::Request sf_post_request {WORKER_URL, sf::Http::Request::Post};
         sf_post_request.setHttpVersion(1, 1);
         sf::Http::Request sf_get_request {WORKER_URL, sf::Http::Request::Get};
         sf_get_request.setHttpVersion(1, 1);
-        sf::Http http;
+        sf::Http http{ WORKER_URL };
         sf::Http::Response response;
 
         string users_maze_output{};
@@ -58,6 +64,7 @@ void process_commands(std::deque<char>& commands,
             cout << "Enter rows, columns, depth, seed, and algorithm with a single space between: ";
             int rows, columns, depth, seed;
             string algorithm;
+            // Get user input
             cin >> rows >> columns >> depth >> seed >> algorithm;
             mazes::maze_types mt = mazes::maze_types::DFS;
             if (algorithm.compare("dfs") == 0) {
@@ -70,14 +77,16 @@ void process_commands(std::deque<char>& commands,
                 cerr << "Invalid algorithm. Please try again.\n";
                 break;
             }
-
+            my_maze_builder->clear();
+            my_maze_builder = std::make_shared<mazes::maze_builder>(rows, columns, depth);
+            rng.seed(seed);
             users_maze_output = my_maze_builder->to_str(mt, cref(get_int), cref(rng), true);
             // @TODO send URL params or JSON body
             sf_post_request.setBody(users_maze_output);
             sf_post_request.setField("Content-Type", "text/plain");
             sf_post_request.setField("Content-Length", to_string(users_maze_output.size()));
 
-            cout << "Sent new maze:{\nrows=" << rows << ";columns=" << columns << ";depth=" << depth << ";seed=" << seed << ";algorithm='" << algorithm << "';str='" << users_maze_output << "';\n}\n";
+            cout << "Sent new maze:{\n\trows=" << rows << ";columns=" << columns << ";depth=" << depth << ";seed=" << seed << ";algorithm='" << algorithm << "';str=\n'" << users_maze_output << "';\n}\n";
 
             response = http.sendRequest(sf_post_request);
             if (response.getStatus() != sf::Http::Response::Ok) {
@@ -102,19 +111,20 @@ void process_commands(std::deque<char>& commands,
             break;
             case '4': {
             // Show example
-            cout << "Showing example maze creation...\n";
+            cout << "Showing example maze builder...\n";
             ostringstream option_1;
             option_1 << "-- Enter rows, columns, depth, seed, and algorithm with a single space between. --\n";
             ostringstream example_user_input;
-            example_user_input << "[1 2 3 1 dfs]\nSent new maze using sf::Http:{\n\trows=1;\n\tcolumns=2;\n\tdepth=3;seed=1;\n\talgorithm='dfs';\n}\n";
+            example_user_input << "Example (ignore [] brackets):\t[1 2 3 1 dfs]\nSent new maze using sf::Http:\n{\trows=1;\n\tcolumns=2;\n\tdepth=3;\n\tseed=1;\n\talgorithm='dfs';}\n";
             ostringstream example_1;
             example_1 << option_1.str() << example_user_input.str();
+            cout << example_1.str();
             break;
             }
             case '5':
             cout << "Exiting Maze Builder...\n";
             is_running = false;
-            return;
+            break;
             default:
             cout << "Invalid option. Please try again.\n";
             break;
@@ -122,9 +132,14 @@ void process_commands(std::deque<char>& commands,
     };
 
     while (!commands.empty()) {
-        auto ch = commands.front();
-        process_char(ch);
-        commands.pop_front();
+        if (auto ch = commands.front()) {
+            commands.pop_front();
+            process_char(ch);
+        }
+        //commands.clear();
+        if (is_running) {
+            print_menu();
+        }
     }
 }
 
@@ -134,17 +149,13 @@ void process_commands(std::deque<char>& commands,
 int main()
 {
     using namespace std;
-    
-    // SFML HTTP Request
-    const string WORKER_URL {"hello-wrangler.zach-mm035.workers.dev"};
 
-    double accumulator = 0.0;
-    static constexpr auto FIXED_TIMESTEP = 1.0 / 60.0;
-
-    // 5000 ms to query from the DB
-    static constexpr auto REQUEST_INTERVAL = 5000;
+    float accumulator = 0.0;
+    static constexpr auto FIXED_TIMESTEP = 1.0f / 60.0f;
 
     sf::Clock clock;
+
+    print_menu();
 
     deque<char> commands;
     bool is_running = true;
@@ -154,14 +165,15 @@ int main()
         accumulator += clock.restart().asSeconds();
         while (accumulator >= FIXED_TIMESTEP) {
             cin.clear();
-            if (auto ch = cin.get()) {
+            if (auto ch = cin.get(); ch != '\n' && ch != EOF) {
                 commands.push_back(ch);
             }
             accumulator -= FIXED_TIMESTEP;
-        }
 
-        // Process commands
-        process_commands(commands, ref(is_running), cref(my_maze_builder), cref(WORKER_URL));
+            // Process commands
+            process_commands(commands, ref(is_running), cref(my_maze_builder));
+        }
+        accumulator = min(accumulator, FIXED_TIMESTEP);
     }
 } // main
 
