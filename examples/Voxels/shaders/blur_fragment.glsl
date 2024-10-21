fed4651e7b0f5e58709cc6400a40781cc09aabf2
zmertens <zach.mm035@gmail.com>
2024-10-21 05:53:44 -0700

    Update shader

--- examples/Voxels/shaders/blur_fragment.glsl
+++ examples/Voxels/shaders/blur_fragment.glsl
@@ -7,7 +7,7 @@ in vec2 TexCoords;
 uniform sampler2D image;
 
 uniform bool horizontal;
-uniform float weight[5] = float[] (0.2270270270, 0.1945945946, 0.1216216216, 0.0540540541, 0.0162162162);
+uniform float weight[5];
 
 void main()
 {             
@@ -17,16 +17,16 @@ void main()
      {
          for(int i = 1; i < 5; ++i)
          {
-            result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-            result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+            result += texture(image, TexCoords + vec2(tex_offset.x * float(i), 0.0)).rgb * weight[i];
+            result += texture(image, TexCoords - vec2(tex_offset.x * float(i), 0.0)).rgb * weight[i];
          }
      }
      else
      {
          for(int i = 1; i < 5; ++i)
          {
-             result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-             result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+             result += texture(image, TexCoords + vec2(0.0, tex_offset.y * float(i))).rgb * weight[i];
+             result += texture(image, TexCoords - vec2(0.0, tex_offset.y * float(i))).rgb * weight[i];
          }
      }
      FragColor = vec4(result, 1.0);
