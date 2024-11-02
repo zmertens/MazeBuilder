699753b8f28887703b00336a61d808c9d9f5fb7b
zmertens <zach.mm035@gmail.com>
2024-11-02 04:07:15 -0700

    Update shaders

--- examples/Voxels/shaders/es/block_fragment.es.glsl
+++ examples/Voxels/shaders/es/block_fragment.es.glsl
@@ -2,7 +2,6 @@
 precision mediump float;
 
 uniform sampler2D sampler;
-uniform sampler2D sky_sampler;
 uniform float timer;
 uniform float daylight;
 uniform bool is_ortho;
@@ -28,18 +27,18 @@ void main() {
     if (cloud && is_ortho) {
         discard;
     }
-    float df = cloud ? 1.0 - diffuse * 0.2 : diffuse;
-    float ao = cloud ? 1.0 - (1.0 - fragment_ao) * 0.2 : fragment_ao;
+    float df = cloud ? diffuse * 1.2 : diffuse;
+    float ao = cloud ? fragment_ao * 1.2 : fragment_ao;
 
-    ao = min(1.0, ao + fragment_light);
-    df = min(1.0, df + fragment_light);
-    float value = min(1.0, daylight + fragment_light);
+    ao += fragment_light;
+    df += fragment_light;
+    float value = daylight + fragment_light;
     vec3 light_color = vec3(value * 0.3 + 0.2);
     vec3 ambient = vec3(value * 0.3 + 0.2);
     vec3 light = ambient + light_color * df;
-    color = clamp(color * light * ao, vec3(0.0), vec3(1.0));
-    vec3 sky_color = texture(sky_sampler, vec2(timer, fog_height)).rgb;
-    color = mix(color, sky_color, fog_factor);
+    color *= light * ao;
+    vec3 fog_mix = vec3(timer, fog_height, 0.0);
+    color = mix(color, vec3(0.0), fog_mix.y * fog_factor);
     fragColor = vec4(color, 1.0);
     
     float brightness = dot(fragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
--- examples/Voxels/shaders/es/sky_fragment.es.glsl
+++ examples/Voxels/shaders/es/sky_fragment.es.glsl
@@ -1,4 +1,5 @@
-#version 330 core
+#version 300 es
+precision mediump float;
 
 layout (location = 0) out vec4 fragColor;
 layout (location = 1) out vec4 brightColor;
