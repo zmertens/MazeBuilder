a180fb0eddb50eb2f8de336ae3eb329e08e2a177
zmertens <zach.mm035@gmail.com>
2025-02-16 08:43:15 -0700

    Refactor writer

--- sources/writer.cpp
+++ sources/writer.cpp
@@ -1,81 +1,126 @@
-/**
- * Writer class handles stdout, and basic file writing to text or Wavefront obj file
- */
-
 #include <MazeBuilder/writer.h>
 
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <stdexcept>
-#include <cassert>
 
 #include <MazeBuilder/enums.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 
-using namespace std;
 using namespace mazes;
 
-writer::writer() {
+/// @brief Hidden implementation for the writer class
+class writer::writer_impl {
+public:
+    explicit writer_impl() = default;
 
+    /// @brief Write a PNG file
+    /// @param filename 
+    /// @param data 
+    /// @param w 1
+    /// @param h 1
+    /// @return 
+    bool write_png(const std::string& filename, const std::string& data, unsigned int w = 1, unsigned int h = 1) const {
+
+        static constexpr auto STRIDE = 4u;
+
+        return (0 != stbi_write_png(filename.c_str(), w, h, STRIDE, data.data(), w * STRIDE));
+    }
+
+    /// @brief Write a JPEG file
+    /// @param filename 
+    /// @param data 
+    /// @param w 
+    /// @param h 
+    /// @return 
+    bool write_jpeg(const std::string& filename, const std::string& data, unsigned int w = 1, unsigned int h = 1) const {
+
+        static constexpr auto STRIDE = 4u;
+
+        return (0 != stbi_write_jpg(filename.c_str(), w, h, STRIDE, data.data(), w * STRIDE));
+    }
+
+    /// @brief Write to a conventional file
+    /// @param filename 
+    /// @param data 
+    /// @return 
+    bool write_file(const std::string& filename, const std::string& data) const {
+        using namespace std;
+
+        filesystem::path data_path{ filename };
+
+        ofstream out_writer{ data_path };
+
+        out_writer << data;
+
+        out_writer.close();
+
+        return true;
+    }
+
+private:
+
+};
+
+writer::writer() : m_impl{ std::make_unique<writer_impl>() } {
 }
 
-outputs writer::get_output_type(const std::string& filename) const noexcept {
-	if (filename.compare("stdout") == 0) {
-		return outputs::STDOUT;
-	}
+writer::~writer() = default;
 
-	// size of file extension is always 4: <dot>[txt|obj|png]
-	static constexpr auto FILE_EXT_LEN = 4;
-	auto found = filename.find(".");
-	if (found == string::npos) {
-		return outputs::TOTAL;
-	}
-	auto short_str = filename.substr(found, string::npos);
-	if (short_str.length() == FILE_EXT_LEN && short_str == ".txt") {
-		return outputs::PLAIN_TEXT;
-	}
-	else if (short_str.length() == FILE_EXT_LEN && short_str == ".obj") {
-		return outputs::WAVEFRONT_OBJECT_FILE;
-	}
-	else if (short_str.length() == FILE_EXT_LEN && short_str == ".png") {
-		return outputs::PNG;
-	}
-	else {
-        return outputs::TOTAL;
-	}
-}
+/// @brief Main function for handling file I/O
+/// @param filename 
+/// @param data
+/// @param w 1
+/// @param h 1
+/// @return 
+bool writer::write(const std::string& filename, const std::string& data, unsigned int w, unsigned int h) const noexcept {
+    using namespace std;
 
-/**
- * @brief writer::write
- * @param filename can be stdout, .txt, .png, or .obj
- * @param data represents a grid in plain text (ASCII) or Wavefront object file (mesh) or PNG (bytes)
- * @return
- */
-bool writer::write(const std::string& filename, const std::string& data) const noexcept {
-    auto otype = get_output_type(filename);
+    static constexpr auto det_file_by_suffix = [](auto f)->outputs {
 
-	// open file stream and start writing the data as per the file type
+        static constexpr auto MAX_FILE_EXT_LEN = 5;
+
+        // Verify file has valid extension
+        auto found = f.find(".");
+        if (found == string::npos) {
+            throw invalid_argument("Invalid filename: " + f);
+        }
+
+        // Determine file type by the extension
+        auto short_str = f.substr(found, string::npos);
+        if (short_str.length() <= MAX_FILE_EXT_LEN && short_str == ".txt") {
+            return outputs::PLAIN_TEXT;
+        } else if (short_str.length() <= MAX_FILE_EXT_LEN && short_str == ".obj") {
+            return outputs::WAVEFRONT_OBJECT_FILE;
+        } else if (short_str.length() <= MAX_FILE_EXT_LEN && short_str == ".png") {
+            return outputs::PNG;
+        } else if (short_str.length() <= MAX_FILE_EXT_LEN && short_str == ".jpeg") {
+            return outputs::JPEG;
+        } else if (short_str.length() <= MAX_FILE_EXT_LEN && short_str == ".jpg") {
+            return outputs::JPEG;
+        } else {
+            return outputs::STDOUT;
+        }
+
+        }; // lambda
+
+
+    // Handle the output type
 	try {
-		if (otype == outputs::PLAIN_TEXT) {
-			this->write_file(filename, data);
-			return true;
-		} else if (otype == outputs::WAVEFRONT_OBJECT_FILE) {
-			this->write_file(filename, data);
-			return true;
-		} else if (otype == outputs::STDOUT) {
-			cout << data << endl;
-			return true;
-		} else if (otype == outputs::PNG) {
-			cerr << "ERROR: Incorrect arguments for write_png with output: " << filename << "\n";
-			return false;
-		} else if (otype == outputs::TOTAL) {
-			cerr << "ERROR: Unknown output type!!\n";
-			return false;
+        auto ftype = det_file_by_suffix(filename);
+
+		if (ftype == outputs::PLAIN_TEXT) {
+			return this->m_impl->write_file(filename, data);
+		} else if (ftype == outputs::WAVEFRONT_OBJECT_FILE) {
+			return this->m_impl->write_file(filename, data);
+		} else if (ftype == outputs::PNG) {
+            return this->m_impl->write_png(filename, data);
+		} else if (ftype == outputs::JPEG) {
+            return this->m_impl->write_jpeg(filename, data);
 		} else {
-			cerr << "ERROR: Unknown output type!!\n";
 			return false;
 		}
 	} catch (...) {
@@ -83,26 +128,7 @@ bool writer::write(const std::string& filename, const std::string& data) const n
 	}
 }
 
-/**
- * @brief writer::write_file
- * @param filename
- * @param data
- * @param w = 1
- * @param h = 1
- */
-bool writer::write_png(const std::string& filename, const std::vector<std::uint8_t>& data,
-	const unsigned int w, const unsigned int h) const {
-	return stbi_write_png(filename.c_str(), w, h, 4, data.data(), w * 4);
-}
-
-/**
- * @brief writer::write_file
- * @param filename
- * @param data
- */
-void writer::write_file(const std::string& filename, const std::string& data) const {
-    filesystem::path data_path {filename};
-    ofstream out_writer {data_path};
-	out_writer << data;
-	out_writer.close();
+bool writer::write(std::ostream& oss, const std::string& data) const noexcept {
+    oss << data << "\n";
+    return true;
 }
--- tests/test_writer.cpp
+++ tests/test_writer.cpp
@@ -1,49 +1,63 @@
-#include <random>
 #include <vector>
 #include <string>
 #include <memory>
+#include <iosfwd>
+#include <fstream>
+#include <sstream>
 
 #include <catch2/catch_test_macros.hpp>
 
 #include <MazeBuilder/writer.h>
-#include <MazeBuilder/maze_builder.h>
 
 using namespace std;
 using namespace mazes;
 
-TEST_CASE("Writer can receive program arguments", "[determine output format]") {
+TEST_CASE("Writer can process good text file names", "[good text filenames]") {
 	writer my_writer;
-	
-	vector<string> good_filenames{ "1.txt", "2.txt", "3.txt", "1.obj", "2.obj", "7.png" };
 
-	vector<string> bad_filenames{ "1.text", "2.plain_text", "3.plain_txt", "1.object", "2.objobj", "3obj", "a.ping", ""};
+    // Good file names that the writer can determine what type to write per the extension
+	vector<string> good_filenames{ "1.txt", "1.obj", ".obj", ".txt", ".png", "my.jpg", "other.jpeg" };
 
-	for (auto&& good : good_filenames) {
-		auto&& ftype = my_writer.get_output_type(good);
-		CHECK_FALSE(ftype == outputs::TOTAL);
+	for (const auto& gf : good_filenames) {
+        REQUIRE(my_writer.write(gf, "data"));
 	}
-
-	for (auto&& bad : bad_filenames) {
-		auto&& ftype = my_writer.get_output_type(bad);
-		CHECK(ftype == outputs::TOTAL);
-	}
-
-	REQUIRE(my_writer.get_output_type("stdout") == outputs::STDOUT);
 }
 
-TEST_CASE("Writer can produce a PNG file", "[does png]") {
-    // mazes::builder builder;
-    // unique_ptr<maze> my_maze = builder.rows(10).columns(10).height(10).build();
-	// progress p{};
-	// computations::compute_geometry(cref(my_maze));
-	// auto elapsed = p.elapsed_s();
-	// auto&& my_png = my_maze->to_pixels(15);
+TEST_CASE("Writer can process bad file names", "[bad filenames]") {
+    writer my_writer;
 
-	// REQUIRE(!my_png.empty());
+    vector<string> bad_filenames{ "1.text", "2.plain_text", "3.plain_txt", "1.object", "2.objobj", "3obj", "a.ping", "" };
 
-	// writer my_writer;
-
-	// REQUIRE(my_writer.get_output_type("1.png") == outputs::PNG);
-
-	// REQUIRE(my_writer.write_png("1.png", my_png, my_maze->rows * 4, my_maze->columns * 4));
+    for (const auto& bf : bad_filenames) {
+        REQUIRE_FALSE(my_writer.write(bf, "data"));
+    }
 }
+
+TEST_CASE("writer::write writes data to file successfully", "[writer writes]") {
+    mazes::writer w;
+    std::string filename = "test_file.txt";
+    std::string data = "Hello, world!";
+
+    REQUIRE_NOTHROW(w.write(filename, data));
+
+    // Verify the file contents
+    std::ifstream f1 (filename);
+    REQUIRE(f1.is_open());
+    std::string f1_content((std::istreambuf_iterator<char>(f1)), std::istreambuf_iterator<char>());
+    REQUIRE(f1_content == data);
+
+    // Clean up
+    f1.close();
+    std::remove(filename.c_str());
+}
+
+TEST_CASE("Writer writes data to stdout successfully", "[writer verifies writes]") {
+    mazes::writer w;
+    std::string data = "Hello, world!";
+    std::ostringstream oss;
+
+    REQUIRE_NOTHROW(w.write(ref(oss), data));
+
+    REQUIRE(oss.str() == data + "\n");
+}
+
