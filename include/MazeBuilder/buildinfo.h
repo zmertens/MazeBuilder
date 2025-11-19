f715d9f7194546dc0345b931dd2590ca48742ff2
zmertens <zach.mm035@gmail.com>
2025-11-19 07:53:55 -0700

    update buildinfo

--- include/MazeBuilder/buildinfo.h
+++ include/MazeBuilder/buildinfo.h
@@ -11,9 +11,9 @@ namespace mazes
     /// @brief Build information for the maze generation library
     struct buildinfo
     {
-        static inline const std::string CommitSHA = "'3be4d97'";
+        static inline const std::string CommitSHA = "'601b66b'";
 
-        static inline const std::string Timestamp = "2025-11-03T06:55:46";
+        static inline const std::string Timestamp = "2025-11-19T07:53:03";
         
         static inline const std::string Version = "7.6.6";
     };
