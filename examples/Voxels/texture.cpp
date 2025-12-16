c79a84d0dafab1baf6d2e42680dc4ca304f566a1
zmertens <zach.mm035@gmail.com>
2025-12-15 23:54:55 -0700

    modify texture loading

--- examples/Voxels/texture.cpp
+++ examples/Voxels/texture.cpp
@@ -96,7 +96,7 @@ bool texture::load_from_file(const std::string_view filepath, const std::uint32_
     glBindTexture(GL_TEXTURE_2D, m_texture);
 
     // Set texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
