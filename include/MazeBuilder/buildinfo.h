5b224ee133c4f5dc058cbeb1f3d269492a9efe26
zmertens <zach.mm035@gmail.com>
2025-12-21 08:23:17 -0700

    version major inc

--- include/MazeBuilder/buildinfo.h
+++ include/MazeBuilder/buildinfo.h
@@ -11,11 +11,11 @@ namespace mazes
     /// @brief Build information for the maze generation library
     struct buildinfo
     {
-        static inline const std::string CommitSHA = "'18f6ee4'";
+        static inline const std::string CommitSHA = "'64b6627'";
 
-        static inline const std::string Timestamp = "2025-12-11T09:11:34";
+        static inline const std::string Timestamp = "2025-12-21T08:20:55";
         
-        static inline const std::string Version = "7.7.5";
+        static inline const std::string Version = "7.8.1";
     };
 
 }
